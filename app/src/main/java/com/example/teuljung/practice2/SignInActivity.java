5943e3cdb988c81aad8beebe4f706736d31a0cc0
teul Jung <xodmf1215@gmail.com>
2016-11-26 15:06:55 +0900

    basic

---
+++ app/src/main/java/com/example/teuljung/practice2/SignInActivity.java
@@ -0,0 +1,20 @@
+package com.example.teuljung.practice2;
+
+/**
+ * Created by teul jung on 2016-11-25.
+ */
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class SignInActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/teuljung/practice2/SignUpActivity.java
@@ -0,0 +1,18 @@
+package com.example.teuljung.practice2;
+
+/**
+ * Created by teul jung on 2016-11-25.
+ */
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class SignUpActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+
+}
